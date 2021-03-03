2920282674774f9536145639f747b6b41020fd48
1981Bareira <renatotom@gmail.com>
2021-03-03 16:29:13 -0300

    commit initial

---
+++ go.mod
@@ -0,0 +1,5 @@
+module fisrtAppGo
+
+go 1.16
+
+require github.com/urfave/cli v1.22.5
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fisrtAppGo/app"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+
+	fmt.Println("Ponto de Partida!!")
+
+	aplicacao := app.Gerar()
+	erro := aplicacao.Run(os.Args)
+
+	if erro != nil {
+
+		log.Fatal(erro)
+	}
+}
